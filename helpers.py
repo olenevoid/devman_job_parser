94b6f8c3193ed38cdaec8d4638f6b276f6fec196
olenevoid <mazekunov@gmail.com>
2025-07-01 16:02:46 +0500

    Add load from json to helpers

--- helpers.py
+++ helpers.py
@@ -5,3 +5,9 @@ def save_readable_json(dictionary, filename):
     json_string = json.dumps(dictionary, indent=4, ensure_ascii=False)
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(json_string)
+
+
+def load_from_json(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = file.read()
+        return json.loads(data)
