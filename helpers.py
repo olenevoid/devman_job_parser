62fa3ac81fe12b5ecbd2c9f3537be570c2080ac0
olenevoid <mazekunov@gmail.com>
2025-07-01 16:07:02 +0500

    Add print_as_table

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import json
+from terminaltables import AsciiTable
 
 
 def save_readable_json(dictionary, filename):
@@ -11,3 +12,30 @@ def load_from_json(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         data = file.read()
         return json.loads(data)
+
+
+def print_as_table(salaries, title):
+    table_headers = [
+        'Язык программирования',
+        'Вакансий с зарплатой',
+        'Вакансий обработано',
+        'Средняя зарплата'
+    ]
+    
+    table_data = [
+        table_headers,
+    ]
+
+    for lang, salary in salaries.items():
+        row = [
+            lang,
+            salary['total'],
+            salary['processed'],
+            salary['average_salary']
+        ]
+
+        table_data.append(row)
+    
+    table = AsciiTable(table_data, title)
+    
+    print(table.table)
